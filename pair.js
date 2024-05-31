8ad70b40d6f66f3dca03017cecdb564b721bbc43
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-05-31 16:26:49 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ['Chrome (Linux)', '', ''],
+                browser: Browsers.macOS("Desktop"),
              });
              if(!XeonBotInc.authState.creds.registered) {
                 await delay(1500);
