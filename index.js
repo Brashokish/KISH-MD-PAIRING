a81a3bc81a7ff3fe1d571e48f332b758a774e37c
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-12-18 16:15:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+const code = require('./pair');
+
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star
 
- Kish Pairing Server running on http://localhost:` + PORT)
-})
+app.use('/code', code);
+app.use('/pair', (req, res) => res.sendFile(`${__dirname}/pair.html`));
+app.use('/', (req, res) => res.sendFile(`${__dirname}/main.html`));
 
-module.exports = app
-
+module.exports = app;
