73b9d9822fc674c86d1d9a0ab3f2f91095e3653e
Brashokish <130168063+Brashokish@users.noreply.github.com>
2024-12-18 21:10:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 const express = require('express');
 const app = express();
+__path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-const path = require('path');
-
-// Import the pair.js and qr.js routers
 let code = require('./pair');
-let qr = require('./qr'); // Make sure to create qr.js as shown previously
-
-// Set the maximum number of listeners
 require('events').EventEmitter.defaultMaxListeners = 500;
-
-// Middleware
+app.use('/code', code);
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/main.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-app.use('/code', code);
-app.use('/qr', qr); // Use the QR router
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'pair.html')); // Ensure pair.html is in the same directory
-});
-app.use('/', async (req, res, next) => {
-    res.sendFile(path.join(__dirname, 'main.html')); // Ensure main.html is in the same directory
-});
-
-// Start the server
 app.listen(PORT, () => {
     console.log(`
 Don't Forget To Give Star
 
-Kish Pairing Server running on http://localhost:${PORT}`);
-});
+ Kish Pairing Server running on http://localhost:` + PORT)
+})
 
-module.exports = app;
+module.exports = app
